2a41ee720e26e009e7c620700f338d4459c1e7b6
Bongani Ntshingila <ntshingilab@outlook.com>
2023-02-21 02:09:13 -0800

    2-print_alphabet_x10.c Betty Changes

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,13 +10,12 @@ void print_alphabet_x10(void)
 	char a;
 	int i;
 
-	for(i = 0; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for(a = 'a'; a <= 'z'; a++)
+		for (a = 'a'; a <= 'z'; a++)
 		{
 			_putchar(a);
 		}
-	
 		_putchar('\n');
 	}
 }
