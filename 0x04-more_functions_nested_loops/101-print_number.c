8c5b0cbfabf1537a5d136154b4f20bcb192bb399
Bongani Ntshingila <ntshingilab@outlook.com>
2023-02-23 02:53:19 -0800

    101-print_number.c changes

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,13 +6,21 @@
  * @n: The integer to be printed.
 */
 
-void print_number(int n) {
-    if (n < 0) {
-        _putchar('-');
-        n = -n;
-    }
-    if (n >= 10) {
-        print_number(n / 10);
-    }
-    _putchar(n % 10 + '0');
+void print_number(int n)
+{
+	unsigned int num = n;
+
+	/*first check if its negative*/
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+
+	/*print the first few digits*/
+	if ((num / 10) > 0)
+		print_number(num / 10);
+
+	/*print the last digit*/
+	_putchar((num % 10) + 48);
 }
