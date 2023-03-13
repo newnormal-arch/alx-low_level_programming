4aeb6fd2a34f47b5a0022119b7a3668e64ce34e6
Bongani Ntshingila <ntshingilab@outlook.com>
2023-03-13 03:58:20 -0700

    4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -14,13 +14,13 @@
 void free_grid(int **grid, int height)
 {
 
-	int i;
+int i;
 
-	for (i = 0; i < height; i++)
-		free(grid[i]);
+for (i = 0; i < height; i++)
+free(grid[i]);
 
-		free(grid);
+free(grid);
 
-	return;
+return;
 
 }
